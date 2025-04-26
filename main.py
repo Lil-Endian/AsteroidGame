658d4f821e206c8776890bd6d18feabbd4666a55
Greg Sleeter <gregsleeter@gmail.com>
2025-04-26 02:16:38 +0200

    gee wilikers it can move

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -11,12 +9,27 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while(True):
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+            
+        #pygame.Surface.fill(screen, "black")
+        screen.fill("black")
+
+        #pygame.draw.polygon(screen,"white", player.triangle(), 2)
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
